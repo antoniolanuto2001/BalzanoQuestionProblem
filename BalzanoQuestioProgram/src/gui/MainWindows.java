d9427b80ab9c68dcd2ad7c7bd4bba0699217414d
anton <anton@PC-Antonio>
2022-05-03 00:16:00 +0200

    Scritta Centrale Prova

--- BalzanoQuestioProgram/src/gui/MainWindows.java
+++ BalzanoQuestioProgram/src/gui/MainWindows.java
@@ -3,6 +3,10 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.UIManager;
 
 public class MainWindows {
 
@@ -38,8 +42,23 @@ public class MainWindows {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 1440, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panelMainButton = new JPanel();
+		panelMainButton.setBounds(0, 0, 1440, 800);
+		frame.getContentPane().add(panelMainButton);
+		panelMainButton.setLayout(null);
+		
+		JLabel testLabel1 = new JLabel("Ciao Erasmo ed Biagio");
+		testLabel1.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		testLabel1.setBounds(143, 106, 143, 41);
+		panelMainButton.add(testLabel1);
+		
+		JLabel testLabel2 = new JLabel("<3 ");
+		testLabel2.setFont(new Font("Segoe UI", Font.BOLD, 17));
+		testLabel2.setBounds(193, 141, 41, 35);
+		panelMainButton.add(testLabel2);
 	}
-
 }
