9b0b28af845cab870be93b0bcc749fe719c77ca2
anton <anton@PC-Antonio>
2022-05-03 00:32:53 +0200

    Commit Prova

--- BalzanoQuestioProgram/src/gui/MainWindows.java
+++ BalzanoQuestioProgram/src/gui/MainWindows.java
@@ -17,7 +17,7 @@ public class MainWindows {
 	 * @author Antonio Lanuto
 	 * @author Erasmo Prosciutto
 	 * @author Biagio Scotto
-	 * ciao tommso
+	 * Ciao tommaso come stai 
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
