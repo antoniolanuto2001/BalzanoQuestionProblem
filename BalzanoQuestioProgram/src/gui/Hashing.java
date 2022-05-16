48a0a6848da7ada2490252290265315aa8eeae2c
anton <anton@PC-Antonio>
2022-05-16 14:32:44 +0200

    GUI hashing delle prime funzioni

--- BalzanoQuestioProgram/src/gui/Hashing.java
+++ BalzanoQuestioProgram/src/gui/Hashing.java
@@ -11,7 +11,9 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,6 +22,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
 import model.parolaHashing;
+import javax.swing.SwingConstants;
+import javax.swing.JRadioButton;
 
 public class Hashing extends JFrame {
 
@@ -35,51 +39,11 @@ public class Hashing extends JFrame {
 		String [] dizionarioParola = {"Vulcano","Amico","Organo","Rame","Gorilla","Orzo","Noci","Melanzana","Cristallo","Pavimento","Risotto","Sei","Stalattiti","Sinistra",
 				"Siluro","Lavandino","Furbo","Moneta","Lampada","Camomilla","Proboscide","Russo","Blu","Valigia","Gorilla","Macchia","Grotta","Mutande","Pasta",
 				"Cornice","Oggetto","Incrocio","Pannocchia","Scarpa","Giorno","Computer","Scaffale","Tanga","Pasta","Contenitore","Medicina","Sensuale","Viso","Elettricità","Canile"};
-		
-		System.out.println("String lenght "+dizionarioParola.length);//45
 		ArrayList<parolaHashing> listaParolaHashings = new ArrayList<parolaHashing>();
 		Random randomGenerator = new Random();
-		int indexDaEstrarre = randomGenerator.nextInt(44 - 0 + 1) + 0;
-		System.out.println("Random Number "+indexDaEstrarre);//45
-		boolean tuttook= true;
-		for (int i = 0; i < 15; i++) 
-		{
-			indexDaEstrarre = randomGenerator.nextInt(44 - 0 + 1) + 0;
-			if (listaParolaHashings.size()==0) 
-			{
-				
-				listaParolaHashings.add(new parolaHashing(dizionarioParola[indexDaEstrarre], indexDaEstrarre, i));
-			}
-			else 
-			{
-				while (tuttook) 
-				{
-					
-					for (parolaHashing iteratore : listaParolaHashings) 
-					{
-						if(iteratore.indexString==indexDaEstrarre)
-						{
-							tuttook=false;
-							break;
-						}
-					}
-					if (tuttook==true)
-					{
-						listaParolaHashings.add(new parolaHashing(dizionarioParola[indexDaEstrarre], indexDaEstrarre, i));
-						tuttook=false;
-					}
-					else 
-					{
-						indexDaEstrarre = randomGenerator.nextInt(44 - 0 + 1) + 0;
-						tuttook=true;
-					}
-				}
-				tuttook=true;
-			}
-		}
-		for (parolaHashing iterato : listaParolaHashings) {
-			System.out.println("Parola  "+iterato.indexParola+" : "+ iterato.parola);
-		}
+		
+		
+		
 		frame=this;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1440, 800);
@@ -89,6 +53,7 @@ public class Hashing extends JFrame {
 		setContentPane(contentPane);
 		
 		JPanel panelMainButton = new JPanel();
+		panelMainButton.setToolTipText("Parole base prese da dizionario di sistema\r\n");
 		panelMainButton.setForeground(new Color(153, 204, 255));
 		panelMainButton.setBounds(0, 0, 1440, 800);
 		frame.getContentPane().add(panelMainButton);
@@ -98,15 +63,6 @@ public class Hashing extends JFrame {
 		panelSuperiore.setBounds(0, 0, 1414, 124);
 		panelMainButton.add(panelSuperiore);
 		panelSuperiore.setLayout(null);
-
-		JTextArea txtrUniversitDegliStudi = new JTextArea();
-		txtrUniversitDegliStudi.setBounds(1125, 11, 278, 102);
-		txtrUniversitDegliStudi.setBackground(new Color(153, 204, 255));
-		txtrUniversitDegliStudi.setText("Universit\u00E0 degli studi di Napoli\r\nAnno Accademico: 2021/2022\r\nProfessore: Walter Balzano\r\nDevoloper: Lanuto, Prosciutto, Scotto\r\n");
-		txtrUniversitDegliStudi.setLineWrap(true);
-		txtrUniversitDegliStudi.setFont(new Font("Segoe UI", Font.PLAIN, 14));
-		txtrUniversitDegliStudi.setEditable(false);
-		panelSuperiore.add(txtrUniversitDegliStudi);
 		
 		JLabel labelSchemata = new JLabel(" Hashing ");
 		labelSchemata.setBounds(573, 11, 320, 89);
@@ -127,14 +83,230 @@ public class Hashing extends JFrame {
 			}
 		});
 		buttonIndietro.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		buttonIndietro.setBounds(10, 135, 128, 46);
+		buttonIndietro.setBounds(148, 135, 128, 46);
 		panelMainButton.add(buttonIndietro);
 		
+		JTextArea textParoleBase = new JTextArea();
+		textParoleBase.setFont(new Font("Arial", Font.PLAIN, 15));
+		textParoleBase.setEditable(false);
+		textParoleBase.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		textParoleBase.setBounds(203, 250, 199, 303);
+		panelMainButton.add(textParoleBase);
+		
 		JButton buttonEsci = new JButton("Esci");
+		buttonEsci.setBorder(new LineBorder(new Color(0, 0, 0)));
+		buttonEsci.setBackground(new Color(204, 204, 255));
+		buttonEsci.setFont(new Font("Segoe UI", Font.BOLD, 14));
+		buttonEsci.setLocation(10, 135);
+		buttonEsci.setSize(128, 46);
 		buttonEsci.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
 		});
+		panelMainButton.add(buttonEsci);
+		
+		JPanel panelControllo = new JPanel();
+		panelControllo.setBorder(new LineBorder(new Color(255, 102, 51), 2));
+		panelControllo.setBounds(1117, 135, 274, 605);
+		panelMainButton.add(panelControllo);
+		panelControllo.setLayout(null);
+		
+		JLabel labelgenerale = new JLabel("Plot :");
+		labelgenerale.setVerticalAlignment(SwingConstants.BOTTOM);
+		labelgenerale.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		labelgenerale.setBounds(10, 21, 122, 26);
+		panelControllo.add(labelgenerale);
+		
+		JPanel panelPlot = new JPanel();
+		panelPlot.setBorder(new LineBorder(new Color(204, 0, 0), 2, true));
+		panelPlot.setBounds(10, 48, 254, 169);
+		panelControllo.add(panelPlot);
+		panelPlot.setLayout(null);
+		
+		JButton buttonGeneraEsempio = new JButton("Genera Esempio");
+		
+		buttonGeneraEsempio.setBackground(new Color(153, 255, 102));
+		buttonGeneraEsempio.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		buttonGeneraEsempio.setFont(new Font("Segoe UI", Font.PLAIN, 12));
+		buttonGeneraEsempio.setBounds(69, 78, 127, 34);
+		panelPlot.add(buttonGeneraEsempio);
+		
+		JComboBox comboBoxSceltaNParole = new JComboBox();
+		
+		
+		comboBoxSceltaNParole.setEnabled(false);
+		comboBoxSceltaNParole.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		comboBoxSceltaNParole.setBackground(new Color(255, 255, 255));
+		comboBoxSceltaNParole.setModel(new DefaultComboBoxModel(new String[] {"9","8","7","6", "5", "4"}));
+		comboBoxSceltaNParole.setBounds(146, 41, 50, 21);
+		panelPlot.add(comboBoxSceltaNParole);
+		
+		JLabel labelGrandezza = new JLabel("Grandezza Dizionario : ");
+		labelGrandezza.setEnabled(false);
+		labelGrandezza.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		labelGrandezza.setBounds(10, 40, 126, 22);
+		panelPlot.add(labelGrandezza);
+		
+		JButton buttonPlay = new JButton("\u25B6");
+		buttonPlay.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		buttonPlay.setBounds(111, 123, 39, 35);
+		panelPlot.add(buttonPlay);
+		
+		JPanel panelSomma = new JPanel();
+		panelSomma.setLayout(null);
+		panelSomma.setBorder(new LineBorder(new Color(204, 0, 0), 2, true));
+		panelSomma.setBounds(10, 242, 254, 135);
+		panelControllo.add(panelSomma);
+		
+		JRadioButton radioSommaNormale = new JRadioButton("Somma di Caratteri");
+		radioSommaNormale.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		radioSommaNormale.setBounds(6, 31, 189, 31);
+		panelSomma.add(radioSommaNormale);
+		
+		JRadioButton radioSommaMultipla = new JRadioButton("Somma di Caratteri Quadratica");
+		radioSommaMultipla.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		radioSommaMultipla.setBounds(6, 74, 213, 31);
+		panelSomma.add(radioSommaMultipla);
+		
+		JLabel labelSomma = new JLabel("Somma : ");
+		labelSomma.setVerticalAlignment(SwingConstants.BOTTOM);
+		labelSomma.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		labelSomma.setBounds(10, 216, 122, 26);
+		panelControllo.add(labelSomma);
+		
+		JLabel labelCodifica = new JLabel("Codifica :");
+		labelCodifica.setVerticalAlignment(SwingConstants.BOTTOM);
+		labelCodifica.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		labelCodifica.setBounds(10, 388, 122, 26);
+		panelControllo.add(labelCodifica);
+		
+		JPanel panelCodifica = new JPanel();
+		panelCodifica.setLayout(null);
+		panelCodifica.setBorder(new LineBorder(new Color(204, 0, 0), 2, true));
+		panelCodifica.setBounds(10, 414, 254, 180);
+		panelControllo.add(panelCodifica);
+		
+		JRadioButton radioXmod = new JRadioButton("x Mod 8");
+		radioXmod.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		radioXmod.setBounds(6, 31, 189, 31);
+		panelCodifica.add(radioXmod);
+		
+		JRadioButton rdbtnx = new JRadioButton("(5x + 2 ) mod 8 ");
+		rdbtnx.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		rdbtnx.setBounds(6, 74, 213, 31);
+		panelCodifica.add(rdbtnx);
+		
+		JRadioButton radioSommaMultipla_1_1 = new JRadioButton("(x + J\u00B2) mod 8");
+		radioSommaMultipla_1_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		radioSommaMultipla_1_1.setBounds(6, 115, 213, 31);
+		panelCodifica.add(radioSommaMultipla_1_1);
+		
+		JLabel labelDizionario = new JLabel("Dizionario :");
+		labelDizionario.setFont(new Font("Segoe UI", Font.BOLD, 14));
+		labelDizionario.setBounds(203, 210, 116, 29);
+		panelMainButton.add(labelDizionario);
+		buttonGeneraEsempio.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				int indexDaEstrarre = randomGenerator.nextInt(44 - 0 + 1) + 0;
+				boolean tuttook= true;
+				comboBoxSceltaNParole.setEnabled(true);
+				labelGrandezza.setEnabled(true);
+				listaParolaHashings.removeAll(listaParolaHashings);
+				textParoleBase.setText("");
+				String numeroParoleString = (String) comboBoxSceltaNParole.getSelectedItem();
+				int numeroParoleInt=Integer.valueOf(numeroParoleString);
+				for (int i = 0; i < numeroParoleInt ; i++) 
+				{
+					indexDaEstrarre = randomGenerator.nextInt(44 - 0 + 1) + 0;
+					if (listaParolaHashings.size()==0) 
+					{
+						
+						listaParolaHashings.add(new parolaHashing(dizionarioParola[indexDaEstrarre], indexDaEstrarre, i));
+						textParoleBase.append(listaParolaHashings.get(0).parola+ "\n");
+					}
+					else 
+					{
+						while (tuttook) 
+						{
+							
+							for (parolaHashing iteratore : listaParolaHashings) 
+							{
+								if(iteratore.indexString==indexDaEstrarre)
+								{
+									tuttook=false;
+									break;
+								}
+							}
+							if (tuttook==true)
+							{
+								listaParolaHashings.add(new parolaHashing(dizionarioParola[indexDaEstrarre], indexDaEstrarre, i));
+								textParoleBase.append(listaParolaHashings.get(listaParolaHashings.size()-1).parola+ "\n");
+								tuttook=false;
+							}
+							else 
+							{
+								indexDaEstrarre = randomGenerator.nextInt(44 - 0 + 1) + 0;
+								tuttook=true;
+							}
+						}
+						tuttook=true;
+					}
+				}
+				
+			}
+		});
+		comboBoxSceltaNParole.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				int indexDaEstrarre = randomGenerator.nextInt(44 - 0 + 1) + 0;
+				boolean tuttook= true;
+				comboBoxSceltaNParole.setEnabled(true);
+				labelGrandezza.setEnabled(true);
+				listaParolaHashings.removeAll(listaParolaHashings);
+				textParoleBase.setText("");
+				String numeroParoleString = (String) comboBoxSceltaNParole.getSelectedItem();
+				int numeroParoleInt=Integer.valueOf(numeroParoleString);
+				for (int i = 0; i < numeroParoleInt ; i++) 
+				{
+					indexDaEstrarre = randomGenerator.nextInt(44 - 0 + 1) + 0;
+					if (listaParolaHashings.size()==0) 
+					{
+						
+						listaParolaHashings.add(new parolaHashing(dizionarioParola[indexDaEstrarre], indexDaEstrarre, i));
+						textParoleBase.append(listaParolaHashings.get(0).parola+ "\n");
+					}
+					else 
+					{
+						while (tuttook) 
+						{
+							
+							for (parolaHashing iteratore : listaParolaHashings) 
+							{
+								if(iteratore.indexString==indexDaEstrarre)
+								{
+									tuttook=false;
+									break;
+								}
+							}
+							if (tuttook==true)
+							{
+								listaParolaHashings.add(new parolaHashing(dizionarioParola[indexDaEstrarre], indexDaEstrarre, i));
+								textParoleBase.append(listaParolaHashings.get(listaParolaHashings.size()-1).parola+ "\n");
+								tuttook=false;
+							}
+							else 
+							{
+								indexDaEstrarre = randomGenerator.nextInt(44 - 0 + 1) + 0;
+								tuttook=true;
+							}
+						}
+						tuttook=true;
+					}
+				}
+			}
+		});
+	
 	}
 }
